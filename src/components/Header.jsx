8e918a8c0238c12942943d1e0dd0cbf2130f1553
brooks <bryllepastoral@gmail.com>
2025-07-16 07:48:36 +0800

    Added Header (Not Responsive)

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Header() {
+    const nav_class = 'px-12 py-2 hover:translate-y-[-2px] hover:underline transition';
+  return (
+    <header className="fixed top-0 left-0 right-0 h-20 mt-4 px-16 grid grid-cols-[auto_1fr_auto]">
+        <div className='flex items-center justify-center'>
+            <img src="./src/assets/racoon.png" className='w-16' alt="" />
+        </div>
+
+        <div className='flex items-center justify-center'>
+            <div className='grid grid-cols-4 divide-x-2 border-2 p-2 rounded text-center font-medium cursor-pointer'>
+                <div><nav className={nav_class}>Home</nav></div>
+                <div><nav className={nav_class}>About</nav></div>
+                <div><nav className={nav_class}>Stack</nav></div>
+                <div><nav className={nav_class}>Projects</nav></div>
+            </div>
+        </div>
+
+        <div className='flex items-center justify-center'>
+            <div className='border-2 rounded-full p-2 cursor-pointer'>
+                <img src="./src/assets/dark_mode.png" className='w-5' alt="" />
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <section className='bg-stone-800 w-full h-dvh flex items-center'>
+        <div className="bg-stone-400 w-full h-[50%]  ">
+            <div>
+
+            </div>
+            <div>
+                <img src="" alt="" />
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Home
