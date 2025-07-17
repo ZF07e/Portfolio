34a36eb9fc6a3fb0247b32e5e0b69ccdacbe19a2
brooks <bryllepastoral@gmail.com>
2025-07-17 23:55:58 +0800

    Added about Section

---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function About() {
+  return (
+    <section className='h-dvh w-full border-t-1'>
+        <div className='h-full px-20 flex items-center'>
+            <div className='text-center'>
+                <h2 className='my-auto text-2xl font-bold'>About Me</h2>
+                <p className='mt-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet aperiam reprehenderit mollitia sapiente ad veritatis excepturi vel illo tempora, quae, rem, perferendis nulla neque. Voluptatum itaque a porro quia doloremque!</p>
+                <p className="mt-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur reiciendis dolor magnam deserunt porro. Asperiores, sequi minima suscipit eligendi mollitia ipsum repellat maiores iusto inventore aperiam laborum rem consectetur laudantium!</p>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default About
