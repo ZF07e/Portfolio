560a9c16f6be18a85f771d80d3d9a8d5ae79789f
brooks <bryllepastoral@gmail.com>
2025-07-17 23:49:33 +0800

    Added Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Header from "./components/Header.jsx"
 import Home from "./components/Home.jsx"
+import About from "./components/About.jsx";
+import Stack from "./components/Stack.jsx";
 
 function App() {
   return (
     <>
       <Header />
       <Home />
+      <About />
+      <Stack />
     </>
   )
 }
