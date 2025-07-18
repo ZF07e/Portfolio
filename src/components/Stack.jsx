de975c3808a7e04df1a52fb5bacaed4c7cce775c
brooks <bryllepastoral@gmail.com>
2025-07-18 14:04:54 +0800

    Adjust sizes

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -3,23 +3,23 @@ import Stack_Card from './Stack_Card.jsx'
 
 function Stack() {
   return (
-    <section className='h-dvh border-t-1 py-8'>
+    <section className='h-dvh border-t-1 py-16'>
         <h2 className="text-2xl font-bold text-center">My Stack</h2>
         
-        <section className="grid grid-cols-5 gap-9 px-20">
-            <Stack_Card src={'html.png'} />
-            <Stack_Card src={'css.png'} />
-            <Stack_Card src={'js.png'} />
-            <Stack_Card src={'java.png'} />
-            <Stack_Card src={'cSharp.png'} />
-            <Stack_Card src={'bootstrap.png'} />
-            <Stack_Card src={'jquery.svg'} />
-            <Stack_Card src={'tailwindcss.png'} />
-            <Stack_Card src={'mysql.png'} />
-            <Stack_Card src={'react.png'} />
-            <Stack_Card src={'node.png'} />
-            <Stack_Card src={'express.png'} />
-            <Stack_Card src={'git.png'} />
+        <section className="grid grid-cols-5 gap-9 px-20 mt-12">
+            <Stack_Card src={'html.png'} stack_name={"HTML"} />
+            <Stack_Card src={'css.png'} stack_name={"CSS"} />
+            <Stack_Card src={'js.png'} stack_name={"JavaScript"} />
+            <Stack_Card src={'java.png'} stack_name={"Java"} />
+            <Stack_Card src={'cSharp.png'} stack_name={"C#"} />
+            <Stack_Card src={'bootstrap.png'} stack_name={"Bootstrap"} />
+            <Stack_Card src={'jquery.svg'} stack_name={"jQuery"} />
+            <Stack_Card src={'tailwindcss.png'} stack_name={"TailwindCss"} />
+            <Stack_Card src={'mysql.png'} stack_name={"MySQL"} />
+            <Stack_Card src={'react.png'} stack_name={"React"} />
+            <Stack_Card src={'node.png'} stack_name={"Node JS"} />
+            <Stack_Card src={'express.png'} stack_name={"Express.js"} />
+            <Stack_Card src={'git.png'} stack_name={"Git"} />
             
         </section>
         
--- src/components/Stack_Card.jsx
+++ src/components/Stack_Card.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-function Stack_Card({src}) {
+function Stack_Card({src, stack_name}) {
   return (
-    <div className='w-full h-full flex px-12 pt-8'>
-      <div className=''>
-        <img src={`./src/assets/stacks/${src}`} className='w-16 mx-auto' alt="" />
-        <h4>Lorem Ipsum</h4>
+    <div className='w-full h-full flex justify-center items-center opacity-0 animate-(--animate-slide_up) transform'>
+      <div className='text-center grid grid-rows-2'>
+        <div className='flex justify-center w-25'>
+          <img src={`./src/assets/stacks/${src}`} className='aspect-square w-14' alt="" />
+        </div> 
+        <h4 className='mt-2 font-medium'>{stack_name}</h4>
       </div>
     </div>
   )
