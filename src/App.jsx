62f6e62969296992b28cfe496b401da9c0d5540f
brooks <bryllepastoral@gmail.com>
2025-07-19 23:43:51 +0800

    Added Projects

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import Header from "./components/Header.jsx"
 import Home from "./components/Home.jsx"
 import About from "./components/About.jsx";
 import Stack from "./components/Stack.jsx";
+import Projects from "./components/Projects.jsx";
 
 function App() {
   return (
-    <>
+    <main className="text-neutral-900">
       <Header />
       <Home />
       <About />
       <Stack />
-    </>
+      <Projects />
+    </main>
   )
 }
 
