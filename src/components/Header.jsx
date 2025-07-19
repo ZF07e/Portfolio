6d0af7a2753279d1cca922533d9c6350b3570f3d
brooks <bryllepastoral@gmail.com>
2025-07-19 23:39:48 +0800

    Adjust z-index of Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 function Header() {
     const nav_class = 'px-12 py-2 hover:translate-y-[-2px] hover:underline transition';
   return (
-    <header className="fixed top-0 left-0 right-0 h-20 mt-4 px-16 grid grid-cols-[auto_1fr_auto]">
+    <header className="fixed z-100 top-0 left-0 right-0 h-20 mt-4 px-16 grid grid-cols-[auto_1fr_auto]">
         <div className='flex items-center justify-center'>
             <img src="./src/assets/racoon.png" className='w-16' alt="" />
         </div>
 
         <div className='flex items-center justify-center'>
-            <div className='grid grid-cols-4 divide-x-2 border-2 p-2 rounded text-center font-medium cursor-pointer bg-[url(./src/assets/dot_grid.png)] bg-center bg-cover bg-repeat'>
+            <div className='grid grid-cols-4 divide-x-2 border-2 p-2 rounded text-center font-medium cursor-pointer bg-[url(./src/assets/dot_grid.png)] bg-center bg-cover bg-no-repeat'>
                 <div><nav className={nav_class}>Home</nav></div>
                 <div><nav className={nav_class}>About</nav></div>
                 <div><nav className={nav_class}>Stack</nav></div>
