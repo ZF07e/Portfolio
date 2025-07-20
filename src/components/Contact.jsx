91005c5c854af9a1c46bd45a4ca0c918696289f4
brooks <bryllepastoral@gmail.com>
2025-07-20 20:16:33 +0800

    Revert "Contact Section"
    This reverts commit 555fc6add60ed5f6d15b4f1f63506fbcb4dfda70.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,29 +2,8 @@ import React from 'react'
 
 function Contact() {
   return (
-    <section className='w-full border-t-1 py-16 px-12'>
-      <h2 className="text-2xl mb-4 font-bold text-center">Contact Me!</h2>
-      <div className="grid grid-cols-2">
-        <div className='mx-auto my-auto'>
-          <form action="" className="grid grid-rows-[auto_auto_auto_auto] gap-2">
-            <div>
-              <input placeholder='Name' type="text" name="" id="name" className="bg-neutral-200 rounded-2xl px-4 py-2 w-100" />
-            </div>
-            <div>
-              <input placeholder='Email' type="text" name="" id="email" className="bg-neutral-200 rounded-2xl px-4 py-2 w-100" />
-            </div>
-            <div>
-              <textarea placeholder={"Message"} name="" id="message" className="bg-neutral-200 rounded-2xl resize-none p-4 w-100 h-40"></textarea>
-            </div>
-            <div>
-              <button type="button" className="bg-neutral-800 py-2 px-8 rounded-full text-neutral-50 hover:bg-neutral-900 active:opacity-80 cursor-pointer">Submit</button>
-            </div>
-          </form>
-        </div>
-        <div>
-          <img src="./src/assets/contact_me.png" className='aspect-square w-100 mx-auto' alt="" />
-        </div>
-      </div>
+    <section className='h-dvh'>
+        
     </section>
   )
 }
