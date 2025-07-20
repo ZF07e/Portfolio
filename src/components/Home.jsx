ecc11fe2cb25ec621d000406d031473c327654a8
brooks <bryllepastoral@gmail.com>
2025-07-20 22:50:04 +0800

    Small Fixes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 function Home() {
   return (
     <section className='w-full h-dvh flex items-center'>
-        <div className="w-full h-[50%] grid grid-cols-2 gap-100 border-y-1">
+        <div className="w-full h-[50%] grid grid-cols-2 gap-100 border-y-1 bg-[url(./src/assets/square_grid.png)] bg-cover bg-center b-no-repeat">
             <div className='my-auto ms-auto'>
                 <div>
                     <h2 className='text-3xl font-bold'>Marius Brylle U. Pastoral</h2>
-                    <p className='text-xl font-medium text-neutral-400'>BS Information Technology</p>
+                    <p className='text-xl font-medium text-neutral-500'>BS Information Technology</p>
                     <h5 className='text-xl font-bold'>Front-End Developer</h5>
                 </div>
                 <div>
