62da14b81961ae302ee1ea90cdc9a6eb5e2e3aaa
brooks <bryllepastoral@gmail.com>
2025-07-22 11:54:17 +0800

    Footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import ContactLinks from './ContactLinks.jsx';
 
 function Footer() {
+  const date = new Date();
+  const year = date.getFullYear();
+  const copyright_format = `© ${year} Brylle's Portfolio. All Rights Reserved.`;
+
+
   return (
-    <footer>
-        
+    <footer className='h-50 w-full bg-neutral-200 py-4 px-8 border-t-1 border-neutral-400 text-center'>
+      <div className='w-full h-full flex flex-col items-center justify-center'>
+        <ContactLinks />
+        <p className='mt-2'>{copyright_format}</p>
+      </div>
     </footer>
   )
 }
