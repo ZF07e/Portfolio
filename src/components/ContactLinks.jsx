78f1367ffefd35c71ee513c07295b28822c0e523
brooks <bryllepastoral@gmail.com>
2025-07-24 16:09:24 +0800

    Changed the responsive behavior of the component and removed image elements

--- src/components/ContactLinks.jsx
+++ src/components/ContactLinks.jsx
@@ -2,23 +2,23 @@ import React from 'react'
 
 function ContactLinks() {
   return (
-    <div className='w-full flex items-center justify-center'>
-      <div className='grid grid-cols-[auto_auto_auto] gap-4'>
+    <div className='flex'>
+      <div className='grid grid-cols-[auto_auto_auto] gap-2'>
         <nav className='rounded-lg '>
-          <a href="https://www.linkedin.com/in/bryllemarius/" target='_blank' className='flex justify-center'>
-            <img src="./src/assets/link_logo/linkedin.png" className='w-8' alt="" />
+          <a href="https://www.linkedin.com/in/bryllemarius/" target='_blank' 
+             className='flex justify-center w-8 aspect-square bg-[url(./src/assets/link_logo/linkedin.png)] dark:bg-[url(./src/assets/link_logo/linkedin_light.png)] bg-center bg-cover bg-no-repeat'>
           </a>
         </nav>
 
         <nav className='rounded-lg'>
-          <a href="https://github.com/ZF07e" target='_blank' className='flex justify-center'>
-            <img src="./src/assets/link_logo/github.png" className='w-8' alt="" />
+          <a href="https://github.com/ZF07e" target='_blank' 
+             className='flex justify-center w-8 aspect-square bg-[url(./src/assets/link_logo/github.png)] dark:bg-[url(./src/assets/link_logo/github_light.png)] bg-center bg-cover bg-no-repeat'>
           </a>
         </nav>
 
         <nav className='rounded-lg '>
-          <a href="mailto:bryllepastoral@gmail.com" target='_blank' className='flex justify-center'>
-            <img src="./src/assets/link_logo/email.png" className='w-8' alt="" />
+          <a href="mailto:bryllepastoral@gmail.com" target='_blank' 
+             className='flex justify-center w-8 aspect-square bg-[url(./src/assets/link_logo/email.png)] dark:bg-[url(./src/assets/link_logo/email_light.png)] bg-center bg-cover bg-no-repeat'>
           </a>
         </nav>
       </div>
