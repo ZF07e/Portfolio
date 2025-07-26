48506af0112161cd614d8fba24144e9f5b6a1969
brooks <bryllepastoral@gmail.com>
2025-07-26 14:31:57 +0800

    Added Dark mode Features

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 
 function Contact() {
   return (
-    <section className='w-full border-t-1 py-16 px-12'>
+    <section className='w-full border-t-1 py-16 px-12 dark:text-neutral-100'>
       <h2 className="text-2xl mb-4 font-bold text-center">Contact Me!</h2>
       <div className="grid grid-cols-2">
         <div className='mx-auto my-auto'>
           <form action="" className="grid grid-rows-[auto_auto_auto_auto] gap-2">
             <div>
-              <input placeholder='Name' type="text" name="" id="name" className="bg-neutral-200 rounded-2xl px-4 py-2 w-100" />
+              <input placeholder='Name' type="text" name="" id="name" className="bg-neutral-200 dark:bg-neutral-950 rounded-2xl px-4 py-2 w-100" />
             </div>
             <div>
-              <input placeholder='Email' type="text" name="" id="email" className="bg-neutral-200 rounded-2xl px-4 py-2 w-100" />
+              <input placeholder='Email' type="text" name="" id="email" className="bg-neutral-200 dark:bg-neutral-950 rounded-2xl px-4 py-2 w-100" />
             </div>
             <div>
-              <textarea placeholder={"Message"} name="" id="message" className="bg-neutral-200 rounded-2xl resize-none p-4 w-100 h-40"></textarea>
+              <textarea placeholder={"Message"} name="" id="message" className="bg-neutral-200 dark:bg-neutral-950 rounded-2xl resize-none p-4 w-100 h-40"></textarea>
             </div>
             <div>
-              <button type="button" className="bg-neutral-800 py-2 px-8 rounded-full text-neutral-50 hover:bg-neutral-900 active:opacity-80 cursor-pointer">Submit</button>
+              <button type="button" className="bg-neutral-800 dark:bg-neutral-950 py-2 px-8 rounded-full text-neutral-50 hover:bg-neutral-95  0 active:opacity-80 cursor-pointer">Submit</button>
             </div>
           </form>
         </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,21 +4,21 @@ import portfolio from '../../portfolio-data.json';
 
 function Projects() {
     const projects = portfolio.Projects;
-  return (
-    <section className="py-16 px-20 border-t-1">
-        <div>
-            <h2 className="font-bold text-2xl text-center">Projects</h2>
-        </div>
-        <div className='mt-8 mx-40 grid grid-cols-1 gap-4 '>
-            {
-                projects.map((obj, idx)=>{
-                    return <Project_Card key={idx} info={obj} />
-                })
-            }
-            
-        </div>
-    </section>
-  )
+    return (
+        <section className="py-16 px-20 border-t-1 dark:text-neutral-50">
+            <div>
+                <h2 className="font-bold text-2xl text-center">Projects</h2>
+            </div>
+            <div className='mt-8 mx-40 grid grid-cols-1 gap-4 '>
+                {
+                    projects.map((obj, idx)=>{
+                        return <Project_Card key={idx} info={obj} />
+                    })
+                }
+                
+            </div>
+        </section>
+    )
 }
 
 export default Projects
