147d3e7ab2388b4da500d3011d9ad3eb02600502
brooks <bryllepastoral@gmail.com>
2025-07-28 21:46:26 +0800

    Added proficiency and background during dark mode

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -3,23 +3,23 @@ import Stack_Card from './Stack_Card.jsx'
 
 function Stack() {
   return (
-    <section className='md:h-dvh py-16 dark:text-neutral-50'>
+    <section className='py-16 dark:text-neutral-50'>
         <h2 className="text-2xl font-bold text-center">My Stack</h2>
         
         <section className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-9 px-8 sm:px-12 md:px-20 mt-12">
-            <Stack_Card src={'html.png'} stack_name={"HTML"} />
-            <Stack_Card src={'css.png'} stack_name={"CSS"} />
-            <Stack_Card src={'js.png'} stack_name={"JavaScript"} />
-            <Stack_Card src={'java.png'} stack_name={"Java"} />
-            <Stack_Card src={'cSharp.png'} stack_name={"C#"} />
-            <Stack_Card src={'bootstrap.png'} stack_name={"Bootstrap"} />
-            <Stack_Card src={'jquery.svg'} stack_name={"jQuery"} />
-            <Stack_Card src={'tailwindcss.png'} stack_name={"TailwindCss"} />
-            <Stack_Card src={'mysql.png'} stack_name={"MySQL"} />
-            <Stack_Card src={'react.png'} stack_name={"React"} />
-            <Stack_Card src={'node.png'} stack_name={"Node JS"} />
-            <Stack_Card src={'express.png'} stack_name={"Express.js"} />
-            <Stack_Card src={'git.png'} stack_name={"Git"} />
+            <Stack_Card src={'html.png'} stack_name={"HTML"} proficiency={2} />
+            <Stack_Card src={'css.png'} stack_name={"CSS"} proficiency={2} />
+            <Stack_Card src={'js.png'} stack_name={"JavaScript"} proficiency={2} />
+            <Stack_Card src={'java.png'} stack_name={"Java"} proficiency={2} />
+            <Stack_Card src={'cSharp.png'} stack_name={"C#"} proficiency={2} />
+            <Stack_Card src={'bootstrap.png'} stack_name={"Bootstrap"} proficiency={2} />
+            <Stack_Card src={'jquery.svg'} stack_name={"jQuery"} proficiency={2} />
+            <Stack_Card src={'mysql.png'} stack_name={"MySQL"} proficiency={2} />
+            <Stack_Card src={'tailwindcss.png'} stack_name={"TailwindCss"} proficiency={0} />
+            <Stack_Card src={'react.png'} stack_name={"React"} proficiency={0} />
+            <Stack_Card src={'node.png'} stack_name={"Node JS"} proficiency={0} />
+            <Stack_Card src={'express.png'} stack_name={"Express.js"} proficiency={0} />
+            <Stack_Card src={'git.png'} stack_name={"Git"} proficiency={2} />
             
         </section>
         
--- src/components/Stack_Card.jsx
+++ src/components/Stack_Card.jsx
@@ -1,13 +1,40 @@
 import React from 'react'
 
-function Stack_Card({src, stack_name}) {
+function proficiency_class(stack_proficiency){
+  if(stack_proficiency == "1"){
+    return 'text-red-500';
+  }
+  else if(stack_proficiency == "2"){
+    return 'text-orange-400';
+  }
+  else{
+    return 'text-green-400';
+  }
+}
+
+function proficiency_title(stack_proficiency){
+  if(stack_proficiency == "1"){
+    return 'Expert';
+  }
+  else if(stack_proficiency == "2"){
+    return 'Intermediate';
+  }
+  else{
+    return 'Beginner';
+  }
+}
+
+function Stack_Card({src, stack_name, proficiency}) {
   return (
-    <div className='w-full h-full flex justify-center items-center opacity-0 animate-(--animate-slide_up) transform'>
+    <div className='w-full h-full flex justify-center items-center opacity-0 animate-(--animate-slide_up) transform dark:bg-neutral-900/50 rounded pb-2 pt-4'>
       <div className='text-center grid grid-rows-2'>
         <div className='flex justify-center w-25'>
           <img src={`./src/assets/stacks/${src}`} className='aspect-square w-14' alt="" />
         </div> 
-        <h4 className='mt-2 font-medium'>{stack_name}</h4>
+        <div>
+          <h4 className='mt-2 font-medium'>{stack_name}</h4>
+          <p className={`${proficiency_class(proficiency)} text-sm font-medium`}>{proficiency_title(proficiency)}</p>
+        </div>
       </div>
     </div>
   )
