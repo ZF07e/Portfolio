cec55882aeb87999146b1a51bacfc47b8b54ba39
brooks <bryllepastoral@gmail.com>
2025-08-02 07:34:43 +0800

    Added functionality to ligh/dark mode and added more items

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,9 @@ import {useEffect, useState} from 'react'
 
 // Header component of the website
 function Header() {
+    const html = document.documentElement;
     const [active, setActive] = useState(false);
+    const [mode, setMode] = useState(document.cookie);
 
     const isActive = ()=>{
         if(!active){
@@ -10,6 +12,21 @@ function Header() {
         }
         else{
             setActive(false);
+        }
+    }
+
+    useEffect(()=>{
+        document.cookie = mode;
+        html.dataset.theme = mode.split("=")[1];
+    }, [mode]);
+
+    const change_mode = ()=>{
+        console.log("Mode changed! to " + document.cookie + " " + html.dataset.theme)
+        if(html.dataset.theme == "dark"){
+            setMode("theme=light");
+        }
+        else{
+            setMode("theme=dark");
         }
     }
 
@@ -30,14 +47,17 @@ function Header() {
             <div className='col-span-2 p-2 my-2 rounded md:col-span-1 md:p-0 md:m-0 dark:bg-neutral-950 md:dark:bg-neutral-900'>
                 {/* gap-12 */}
                 <nav className='flex flex-col md:flex-row gap-4 md:gap-8 md:justify-center md:items-center items-end font-roboto font-medium h-full w-full'>   
-                    <a href="">Home</a>
-                    <a href="">Stack</a>
-                    <a href="">Projects</a>
+                    <a href="#Home_section">Home</a>
+                    <a href="#About_section">What i do?</a>
+                    <a href="#stack_section">Stack</a>
+                    <a href="#Project_section">Projects</a>
+                    <a href="#Contact_section">Resume</a>
+                    <a href="#Contact_section">Contact</a>
                 </nav>
             </div>
 
              <div className='hidden md:flex items-center justify-end pe-4 order-2'>
-                 <div className='border dark:border-neutral-50 rounded-full p-2 cursor-pointer'>
+                 <div className='border dark:border-neutral-50 rounded-full p-2 cursor-pointer' onClick={()=>{change_mode()}}>
                      <div className='w-5 h-5 bg-[url(./src/assets/dark_mode.png)] bg-center bg-cover bg-no-repeat dark:bg-[url(./src/assets/light_mode.png)]' ></div>
                  </div>
              </div>
