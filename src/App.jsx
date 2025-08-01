83caa5c214aad990fefb27a03e794782a659b6ce
brooks <bryllepastoral@gmail.com>
2025-08-02 07:35:07 +0800

    Sets theme when first opened

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,20 @@ import Contact from "./components/Contact.jsx";
 import Footer from "./components/Footer.jsx";
 
 function App() {
+  //Check if theme is in localstorage
+  if(!document.cookie){
+      console.log("Cookie setted")
+      document.cookie = "theme=dark";
+  }
+  
+  //Set data attribute of html based on the theme stored in cookies.
+  document.documentElement.setAttribute('data-theme', document.cookie.split('=')[1]);
+
   return (
     <main className="text-neutral-900">
       <Header />
       <Home />
+      <About />
       <Stack />
       <Projects />
       <Contact />
