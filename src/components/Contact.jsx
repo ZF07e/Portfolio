704addb7975d7adcb4c5b810b2a583503e527a5c
brooks <bryllepastoral@gmail.com>
2025-08-01 14:42:59 +0800

    Changed the contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,27 +2,18 @@ import React from 'react'
 
 function Contact() {
   return (
-    <section className='w-full border-t-1 py-16 px-12 dark:text-neutral-100'>
-      <h2 className="text-2xl mb-4 font-bold text-center">Contact Me!</h2>
-      <div className="grid grid-cols-2">
-        <div className='mx-auto my-auto'>
-          <form action="" className="grid grid-rows-[auto_auto_auto_auto] gap-2">
-            <div>
-              <input placeholder='Name' type="text" name="" id="name" className="bg-neutral-200 dark:bg-neutral-950 rounded-2xl px-4 py-2 w-100" />
-            </div>
-            <div>
-              <input placeholder='Email' type="text" name="" id="email" className="bg-neutral-200 dark:bg-neutral-950 rounded-2xl px-4 py-2 w-100" />
-            </div>
-            <div>
-              <textarea placeholder={"Message"} name="" id="message" className="bg-neutral-200 dark:bg-neutral-950 rounded-2xl resize-none p-4 w-100 h-40"></textarea>
-            </div>
-            <div>
-              <button type="button" className="bg-neutral-800 dark:bg-neutral-950 py-2 px-8 rounded-full text-neutral-50 hover:bg-neutral-95  0 active:opacity-80 cursor-pointer">Submit</button>
-            </div>
-          </form>
-        </div>
-        <div>
-          <img src="./src/assets/contact_me.png" className='aspect-square w-100 mx-auto' alt="" />
+    <section className='w-full mb-12 md:mb-20 border-y dark:text-neutral-50 dark:border-none md:px-40'>
+      <div className='bg-[url(./src/assets/dot_grid.png)] dark:bg-[url()] bg-center bg-cover bg-no-repeat'>      
+        <div className='grid grid-rows-2 gap-20 w-full text-center md:grid-rows-1 md:grid-cols-2 md:gap-0 py-12 '>
+          <div className='flex flex-col items-center justify-center order-1 md:order-0'>
+            <img src="./src/assets/contact/email.png" className='aspect-square' alt="" />
+            <a href="mailto:bryllepastoral@gmail.com" target='_blank' className='py-4 px-8 rounded-full bg-neutral-200 dark:bg-neutral-900 font-medium mt-4'>Contact me</a >
+            
+          </div>
+          <div className='flex flex-col items-center justify-center order-0 '>
+            <img src="./src/assets/contact/letter.png" className='aspect-square' alt="" />
+            <a href="./src/assets/resume/Marius Brylle Pastoral.pdf" target='_blank' className='py-4 px-8 rounded-full bg-neutral-200 dark:bg-neutral-900 font-medium mt-4'>My Resume</a>
+          </div>
         </div>
       </div>
     </section>
