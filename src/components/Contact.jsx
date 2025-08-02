377cfb060402b3639e1236b8664d3eceb2f3d21b
brooks <bryllepastoral@gmail.com>
2025-08-02 19:14:18 +0800

    Fixed Background and Path

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import email from '../assets/email.png'
-import letter from '../assets/letter.png'
+import email from '../assets/contact/email.png'
+import letter from '../assets/contact/letter.png'
 
 function Contact() {
   return (
-    <section id='Contact_section' className='w-full mb-12 md:mb-20 border-y dark:text-neutral-50 dark:border-none md:px-40'>
-      <div className='bg-[url(/src/assets/dot_grid.png)] dark:bg-[url()] bg-center bg-cover bg-no-repeat'>      
+    <section id='Contact_section' className='w-full mb-12 md:mb-20 border-y dark:text-neutral-50 dark:border-none'>
+      <div className='bg-[url(/src/assets/dot_grid.png)] dark:bg-[url()] bg-center bg-cover bg-no-repeat md:px-40'>      
         <div className='grid grid-rows-2 gap-20 w-full text-center md:grid-rows-1 md:grid-cols-2 md:gap-0 py-12 '>
           <div className='flex flex-col items-center justify-center order-1 md:order-0'>
             <img src={email} className='aspect-square' alt="" />
@@ -14,7 +14,7 @@ function Contact() {
           </div>
           <div className='flex flex-col items-center justify-center order-0 '>
             <img src={letter} className='aspect-square' alt="" />
-            <a href="/src/assets/resume/Marius Brylle Pastoral.pdf" target='_blank' className='py-4 px-8 rounded-full bg-neutral-200 dark:bg-neutral-900 font-medium mt-4'>My Resume</a>
+            <a href="/Portfolio/resume/Marius Brylle Pastoral.pdf" target='_blank' className='py-4 px-8 rounded-full bg-neutral-200 dark:bg-neutral-900 font-medium mt-4'>My Resume</a>
           </div>
         </div>
       </div>
