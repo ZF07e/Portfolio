f8c3366e4899b92275093f7e14047995bf5c763a
brooks <bryllepastoral@gmail.com>
2025-08-03 10:10:41 +0800

    Change path from portfolio to public

--- src/components/Stack_Card.jsx
+++ src/components/Stack_Card.jsx
@@ -29,7 +29,7 @@ function Stack_Card({src, stack_name, proficiency}) {
     <div className='w-full h-full flex justify-center items-center opacity-0 animate-(--animate-slide_up) transform bg-neutral-200/70 dark:bg-neutral-900/40 rounded pb-2 pt-4'>
       <div className='text-center grid grid-rows-2'>
         <div className='flex justify-center w-25'>
-          <img src={`/Portfolio/stacks/${src}`} className='aspect-square w-14' alt="" />
+          <img src={`/public/stacks/${src}`} className='aspect-square w-14' alt="" />
         </div> 
         <div>
           <h4 className='mt-2 font-medium'>{stack_name}</h4>
